870994a8c0885ba5e5830235d202b32a246da1a6
Trevor Lammers <lammerstrevor@gmail.com>
2024-04-04 14:48:22 -0500

    editing code

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #test2 project, app.py
 # Mike Colbert 09/20/2019
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def index():
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
